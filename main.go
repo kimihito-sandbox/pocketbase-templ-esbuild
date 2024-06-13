85e4ed2f5d5f1b5c84df5d5e85ff843fc92d9e05
kimihito <tatsurotamashiro@gmail.com>
2024-06-13 16:12:41 +0900

    middleware.RemoveTrailingSlash()

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/a-h/templ"
 	"github.com/kimihito-sandbox/pocketbase-templ-esbuild/templates"
 	"github.com/labstack/echo/v5"
+	"github.com/labstack/echo/v5/middleware"
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
@@ -25,22 +26,30 @@ func Render(ctx echo.Context, statusCode int, t templ.Component) error {
 	return ctx.HTML(statusCode, buf.String())
 }
 
-//go:embed assets/dist/*
-var embededAssets embed.FS
+var (
+	//go:embed assets/dist/*
+	embededAssets embed.FS
+
+	distDirFS = echo.MustSubFS(embededAssets, "assets")
+)
+
+func homeHandler(c echo.Context) error {
+	return Render(c, http.StatusOK, templates.Home())
+}
 
 func main() {
 	app := pocketbase.New()
-	// serves static files from the provided public dir (if exists)
+
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
-	e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
-	e.Router.GET("/assets/*", apis.StaticDirectoryHandler(echo.MustSubFS(embededAssets, "assets"), false))
-	e.Router.GET("/home", func(c echo.Context) error {
-		return Render(c, http.StatusOK, templates.Home())
-	})
-	e.Router.GET("/hello/:name", func(c echo.Context) error {
-		name := c.PathParam("name")
-		return c.JSON(http.StatusOK, "Hello, "+name+"!")
-	})
+		e.Router.Pre(middleware.RemoveTrailingSlash())
+
+		e.Router.GET("/*", apis.StaticDirectoryHandler(os.DirFS("./pb_public"), false))
+		e.Router.GET("/assets/*", apis.StaticDirectoryHandler(distDirFS, false))
+		e.Router.GET("/hello", homeHandler)
+		e.Router.GET("/hello/:name", func(c echo.Context) error {
+			name := c.PathParam("name")
+			return c.JSON(http.StatusOK, "Hello, "+name+"!")
+		})
 		return nil
 	})
 	if err := app.Start(); err != nil {
