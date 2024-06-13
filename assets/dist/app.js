d2e7797b1e3554deeaed49a8256bbeae1e37c7bb
kimihito <tatsurotamashiro@gmail.com>
2024-06-13 14:55:10 +0900

    Add assets/dist/ to gitignore

--- assets/dist/app.js
+++
@@ -1,5 +0,0 @@
-(() => {
-  // assets/js/app.js
-  console.log("fooo");
-})();
-//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsiLi4vanMvYXBwLmpzIl0sCiAgInNvdXJjZXNDb250ZW50IjogWyJjb25zb2xlLmxvZyhcImZvb29cIik7XG4iXSwKICAibWFwcGluZ3MiOiAiOztBQUFBLFVBQVEsSUFBSSxNQUFNOyIsCiAgIm5hbWVzIjogW10KfQo=
